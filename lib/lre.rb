08e7ffefe629fabffcae5fc6868156106f5850b8
mharris717 <mharris717@gmail.com>
2011-06-09 11:07:54 -0400

    load config method

--- lib/lre.rb
+++ lib/lre.rb
@@ -1,3 +1,4 @@
+require 'rake'
 require 'fattr'
 %w(file_reload rake_ext).each do |f|
   require File.expand_path(File.dirname(__FILE__)) + "/lre/#{f}.rb"
@@ -8,10 +9,17 @@ module LRE
     fattr(:watch_dirs) do
       ["."]
     end
-    def start!
+    def load_config!
       dir = Dir.getwd
       config_file = "#{dir}/.lre"
-      load(config_file) if FileTest.exist?(config_file)
+      #Thread.new do 
+        puts "starting config load"
+        load(config_file) 
+        puts "loaded config"
+      #end if FileTest.exist?(config_file)
+    end
+    def start!
+      load_config!
       FileReload.run!
       require 'irb'
       IRB.start
